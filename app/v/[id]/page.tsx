42eff3eae6fdcb333e61561a2c7923cd7bb208aa
tompetrefis <168531036+tompetrefis@users.noreply.github.com>
2025-05-04 03:11:08 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -58,7 +58,7 @@ export async function generateMetadata(
             title,
             description,
             images: [...previousOgImages, image],
-            url: `/v/${file.filecode}`,
+            url: `/v/${file.filecode}#${file.title}`,
             type: `article`,
         },
         alternates: {
